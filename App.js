cc1ff2394d220d346aa59f15a532fe2c1c66607e
Vivek Rajkumar <vivek.rajkumar@protonmail.com>
2018-07-29 03:45:32 -0400

    Add UdaciSlider and UdaciStepper components

--- App.js
+++ App.js
@@ -3,44 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
-  state = {
-    run: 0,
-    bike: 0,
-    swim: 0,
-    sleep: 0,
-    eat: 0
-  }
-
-  increment = (metric) => {
-    const { max, step } = getMetricMetaInfo(metric);
-
-    this.setState((currentState) => {
-      const count = currentState[metric] + step;
-
-      return {
-        [metric]: count > max ? max : count
-      };
-    });
-  }
-
-  decrement = (metric) => {
-    const { step } = getMetricMetaInfo(metric);
-
-    this.setState((currentState) => {
-      const count = currentState[metric] - step;
-
-      return {
-        [metric]: count < 0 ? 0 : count
-      };
-    });
-  }
-
-  slide = (metric, value) => {
-    this.setState({
-      [metric]: value
-    });
-  }
-
   render() {
     return (
       <View>
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,75 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers';
+import UdaciSlider from './UdaciSlider';
+import UdaciStepper from './UdaciStepper';
 
 export default class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0
+  }
+
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric);
+
+    this.setState((currentState) => {
+      const count = currentState[metric] + step;
+
+      return {
+        [metric]: count > max ? max : count
+      };
+    });
+  }
+
+  decrement = (metric) => {
+    const { step } = getMetricMetaInfo(metric);
+
+    this.setState((currentState) => {
+      const count = currentState[metric] - step;
+
+      return {
+        [metric]: count < 0 ? 0 : count
+      };
+    });
+  }
+
+  slide = (metric, value) => {
+    this.setState({
+      [metric]: value
+    });
+  }
+
   render() {
+    const metaInfo = getMetricMetaInfo();
+
     return (
       <View>
-        {getMetricMetaInfo('bike').getIcon()}
+        {Object.keys(metaInfo).map((metric) => {
+          const { getIcon, type, ...rest } = metaInfo[metric];
+          const value = this.state[metric];
+
+          return (
+            <View key={metric}>
+              {getIcon()}
+              {type === 'slider'
+                ? <UdaciSlider
+                    value={value}
+                    onChange={(slide) => this.slide(metric, value)}
+                    {...rest}
+                  />
+                : <UdaciStepper
+                    value={value}
+                    increment={() => this.increment(metric)}
+                    decrement={() => this.decrement(metric)}
+                    {...rest}
+                  />}
+            </View>
+          );
+        })}
       </View>
     );
   }
